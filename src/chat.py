6b6a72c08bc3bab007e9631905aebf7f5b086746
Thijs Boom <twboom@hotmail.nl>
2021-03-09 01:35:19 +0100

    Tried some things

---
+++ src/chat.py
@@ -0,0 +1,14 @@
+import klangoo as kg
+import os
+
+os.system('cls' if os.name == 'nt' else 'clear')
+
+def run():
+    question = input()
+    answer = kg.ask(question)
+    print(answer)
+    run()
+
+print('Hello!')
+
+run()
--- src/klangoo.py
+++ src/klangoo.py
@@ -1,15 +1,52 @@
 import json
+import random
 klangoo = {}
 
-with open('intents.json', 'r') as intents:
-    klangoo[intents] = json.load(intents)
+with open('intents.json', 'r') as myIntents:
+    klangoo['intents'] = json.load(myIntents)
 
-with open('responses.json', 'r') as responses:
-    klangoo[responses] = json.load(responses)
+with open('responses.json', 'r') as myResponses:
+    klangoo['responses'] = json.load(myResponses)
+
+def confusion():
+    return random.choice(klangoo['responses']['confused'])
+
+def get_intent(question):
+    intents = list(klangoo['intents'])
+    words = question.split(' ')
+    probability = {}
+    for intent in intents:
+        matches = list(set(klangoo['intents'][intent]).intersection(words))
+        probability[intent] = len(matches)
+    intent = max(probability, key=probability.get)
+    return intent
 
 def ask(question):
-    
-    answer = 'Hello'
-    return answer
+    answer = []
+    ansWordCount = random.randint(3, 9)
+    intents = klangoo['intents']
+    responses = klangoo['responses']
+    if question == '': return confusion()
+    intent = get_intent(question)
 
-print(klangoo)
+    print (intent)
+
+    return ''
+
+    words = question.split(' ')
+    wordList = []
+    for key in responses:
+        for i, word in enumerate(responses[key]):
+            wordList.append(word)
+    
+    for i in range(ansWordCount):
+        thisWord = random.choice(wordList)
+        answer.append(thisWord)
+        answer.append('')
+
+
+    output = ''
+    for word in answer:
+        output += word + ' '
+
+    return output
